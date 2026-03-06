0912273783e0310cfb7c45d89f0d9ed519124174
francisco-canales <canalesf23@gmail.com>
2026-03-06 17:01:49 -0600

    Añadi la clase img-fluid

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c9d56ee77b149f9bbf430a14405fb2d429adc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937f68e191f1b4fb099cdedeb6c2ec0173761798")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
